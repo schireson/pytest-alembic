7048cdc91deb731e871208856771e4d2722fe668
Dan Cardin <ddcardin@gmail.com>
2020-03-02 08:22:49 -0500

    Document and centralize context running comamnds.

--- src/pytest_alembic/runner.py
+++ src/pytest_alembic/runner.py
@@ -1,13 +1,19 @@
 import contextlib
 import functools
 from dataclasses import dataclass
+from typing import List
 
-from pytest_alembic.history import AlembicHistory
 from pytest_alembic.executor import CommandExecutor
+from pytest_alembic.history import AlembicHistory
 
 
 @contextlib.contextmanager
 def runner(config, engine=None):
+    """Manage the alembic execution context, in a given context.
+
+    Yields:
+        `MigrationContext` to the caller.
+    """
     command_executor = CommandExecutor.from_config(config)
     migration_context = MigrationContext(command_executor)
 
@@ -21,9 +27,18 @@ def runner(config, engine=None):
 
 @dataclass
 class MigrationContext:
+    """Within a given environment/execution context, executes alembic commands.
+    """
+
     executor: CommandExecutor
 
     def generate_revision(self, process_revision_directives=None, **kwargs):
+        """Generate a test revision.
+
+        The final act of this process raises a `RevisionSuccess`, which is used as a sentinal
+        to indicate the revision was generated successfully, while not actually finishing the
+        generation of the revision file.
+        """
         fn = RevisionSuccess.process_revision_directives(process_revision_directives)
         try:
             return self.executor.run_command("revision", process_revision_directives=fn, **kwargs)
@@ -32,39 +47,61 @@ class MigrationContext:
 
     @property
     def history(self) -> AlembicHistory:
+        """Get the revision history.
+        """
         raw_history = self.executor.run_command("history")
         return AlembicHistory.parse(raw_history)
 
     @property
-    def heads(self):
+    def heads(self) -> List[str]:
+        """Get the list of revision heads.
+        """
         return self.executor.run_command("heads")
 
-    def run(self, *args, **kwargs):
+    def raw_command(self, *args, **kwargs):
+        """Execute a raw alembic command.
+        """
         return self.executor.run_command(*args, **kwargs)
 
     def migrate_up_before(self, revision):
+        """Upgrade up to, but not including the given `revision`.
+        """
         preceeding_revision = self.history.previous_revision(revision)
-        self.executor.run_command("upgrade", preceeding_revision)
+        self.raw_command("upgrade", preceeding_revision)
 
     def migrate_up_to(self, revision):
+        """Upgrade up to, and including the given `revision`.
+        """
         self.history.validate_revision(revision)
-        self.executor.run_command("upgrade", revision)
+        self.raw_command("upgrade", revision)
 
     def migrate_up_one(self):
-        self.executor.run_command("downgrade", "+1")
+        """Upgrade up by exactly one revision.
+        """
+        self.raw_command("downgrade", "+1")
 
     def migrate_down_before(self, revision):
+        """Upgrade down to, but not including the given `revision`.
+        """
         next_revision = self.history.next_revision(revision)
-        self.run("downgrade", self.get_hash_before(next_revision))
+        self.raw_command("downgrade", self.get_hash_before(next_revision))
 
     def migrate_down_to(self, revision):
+        """Upgrade down to, and including the given `revision`.
+        """
         self.history.validate_revision(revision)
-        self.executor.run_command("downgrade", revision)
+        self.raw_command("downgrade", revision)
 
     def migrate_down_one(self):
-        self.executor.run_command("downgrade", "-1")
+        """Upgrade down by exactly one revision.
+        """
+        self.raw_command("downgrade", "-1")
 
     def roundtrip_next_revision(self):
+        """Upgrade, downgrade then upgrade.
+
+        This is meant to ensure that the given revision is idempotent.
+        """
         self.migrate_up_one()
         self.migrate_down_one()
         self.migrate_up_one()
