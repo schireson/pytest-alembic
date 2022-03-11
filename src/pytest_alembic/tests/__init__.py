f7a5a23cb72d8eb06ef42f4ccfb20d13660b7412
Anatolii Aniskovych <lin.aaa.lin@gmail.com>
2022-03-11 15:13:38 +0000

    Explicitly reexport tests

--- src/pytest_alembic/tests/__init__.py
+++ src/pytest_alembic/tests/__init__.py
@@ -1,7 +1,13 @@
-# flake8: noqa
 from pytest_alembic.tests.default import (
     test_model_definitions_match_ddl,
     test_single_head_revision,
     test_up_down_consistency,
     test_upgrade,
 )
+
+__all__ = [
+    "test_model_definitions_match_ddl",
+    "test_single_head_revision",
+    "test_up_down_consistency",
+    "test_upgrade",
+]
