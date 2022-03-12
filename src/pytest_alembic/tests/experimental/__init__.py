d5375ad3cba6066826c2ac4df3220d20433d381e
Dan Cardin <ddcardin@gmail.com>
2022-03-12 09:23:07 -0500

    fix: Add missing explicit reexports.

--- src/pytest_alembic/tests/experimental/__init__.py
+++ src/pytest_alembic/tests/experimental/__init__.py
@@ -1,7 +1,11 @@
-# flake8: noqa
 from pytest_alembic.tests.experimental.all_models_register_on_metadata import (
     test_all_models_register_on_metadata,
 )
 from pytest_alembic.tests.experimental.downgrade_leaves_no_trace import (
     test_downgrade_leaves_no_trace,
 )
+
+__all__ = [
+    "test_all_models_register_on_metadata",
+    "test_downgrade_leaves_no_trace",
+]
